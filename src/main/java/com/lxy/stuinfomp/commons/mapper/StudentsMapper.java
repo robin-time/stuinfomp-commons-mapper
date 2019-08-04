b0ec697f348065d2fea2564dfe8b805a0cdc9177
robin_time <robin_time@163.com>
2019-08-05 00:11:13 +0800

    coreinfo service student crud

--- src/main/java/com/lxy/stuinfomp/commons/mapper/StudentsMapper.java
+++ src/main/java/com/lxy/stuinfomp/commons/mapper/StudentsMapper.java
@@ -1,7 +1,11 @@
 package com.lxy.stuinfomp.commons.mapper;
 
 import com.lxy.stuinfomp.commons.domain.Students;
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.MyMapper;
 
 public interface StudentsMapper extends MyMapper<Students> {
+
+    @Select("select Max(id) from students")
+    Long selectStudentMaxId();
 }
