e2cbcfba99980daab53616b76fdf665cf0682060
robin_time <robin_time@163.com>
2019-08-12 00:33:41 +0800

    登录优化

--- src/main/java/com/lxy/stuinfomp/commons/mapper/UsersMapper.java
+++ src/main/java/com/lxy/stuinfomp/commons/mapper/UsersMapper.java
@@ -1,7 +1,10 @@
 package com.lxy.stuinfomp.commons.mapper;
 
 import com.lxy.stuinfomp.commons.domain.Users;
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.MyMapper;
 
 public interface UsersMapper extends MyMapper<Users> {
+    @Select("select * from users where user_name = #{name} and is_delete = 0")
+    Users selectByUserName(String name);
 }
